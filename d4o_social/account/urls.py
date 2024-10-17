d3ffcc8ea0393bd20e55a33e39e7753f641b15a4
BlooAM <adam3200@vp.pl>
2024-10-17 09:25:55 +0200

    register dashboard url

--- d4o_social/account/urls.py
+++ d4o_social/account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView, name='login'),
     path('logout/', auth_views.LogoutView, name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
