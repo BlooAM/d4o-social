677ffc909bcd6c467de5b9f422432f7257f3dcd1
BlooAM <adam3200@vp.pl>
2024-10-27 08:49:58 +0100

    fix typo

--- d4o_social/d4o_social/settings.py
+++ d4o_social/d4o_social/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_exntensions',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
