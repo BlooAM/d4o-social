147cb02825ef556373654c3c9d9f56e010130d75
BlooAM <adam3200@vp.pl>
2024-11-05 08:59:57 +0100

    add model indexing

--- d4o_social/images/models.py
+++ d4o_social/images/models.py
@@ -8,3 +8,14 @@ class Image(models.Model):
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='/images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
