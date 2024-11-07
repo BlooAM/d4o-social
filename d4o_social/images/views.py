3c99c97da0242540269bb20e549b8a048cd9bdd4
BlooAM <adam3200@vp.pl>
2024-11-07 06:59:56 +0100

    view handling image create

--- d4o_social/images/views.py
+++ d4o_social/images/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = cd.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image has been uploaded.')
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+
+    return render(request, 'images/image/create.html', {'sections': 'images', 'form': form})
