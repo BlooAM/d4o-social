61ff9a9ffbcf595c8fa4b02d517081fc169df076
BlooAM <adam3200@vp.pl>
2024-11-10 11:30:27 +0100

    add onClick event for images

--- d4o_social/images/static/js/bookmarklet.js
+++ d4o_social/images/static/js/bookmarklet.js
@@ -60,6 +60,18 @@ function bookmarkletLaunch() {
                   '_blank');
     })
   })
+
+  imageFound.querySelectorAll('img').forEach(image => {
+    image.addEventListener('click', function(event){
+        imageSelected = event.target;
+        bookmarklet.style.display = 'none';
+        window.open(siteUrl + 'images/create/?url='
+                            + encodeURIComponent(imageSelected.src)
+                            + '&title='
+                            + encodeURIComponent(document.title),
+                            '_blank');
+    })
+  })
 }
 
 // launch the bookmkarklet
