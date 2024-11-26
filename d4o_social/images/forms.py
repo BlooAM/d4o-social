090f9cd15f0789a10847caf90589ff1d39c2cbc0
BlooAM <adam3200@vp.pl>
2024-11-26 16:17:04 +0100

    image validating fix

--- d4o_social/images/forms.py
+++ d4o_social/images/forms.py
@@ -10,14 +10,14 @@ class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['title', 'url', 'description']
-        widgets = {
-            'url': forms.HiddenInput,
-        }
+        # widgets = {
+        #     'url': forms.HiddenInput,
+        # }
 
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg', 'png']
-        extension = url.split('.', 1)[1].lower()
+        extension = url.split('.')[-1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError('Given URL doesn`t contain image with supported format.')
         return url
