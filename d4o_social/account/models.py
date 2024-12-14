b195694942dbb58aa7d45e2bea8da29b24ca2c55
BlooAM <adam3200@vp.pl>
2024-12-14 10:00:32 +0100

    Contact account modfel

--- d4o_social/account/models.py
+++ d4o_social/account/models.py
@@ -12,6 +12,29 @@ class Profile(models.Model):
         return f'Profile of user {self.user.username}'
 
 
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        'auth.User',
+        related_name='rel_from_set',
+        on_delete=models.CASCADE,
+    )
+    user_to = models.ForeignKey(
+        'auth.User',
+        related_name='rel_to_set',
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
 user_model = get_user_model()
 user_model.add_to_class(
     'following',
