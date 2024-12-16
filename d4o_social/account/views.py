f4056f4101a8210c47d3a0bb47f230bb5eed973d
BlooAM <adam3200@vp.pl>
2024-12-16 09:50:11 +0100

    users detail view

--- d4o_social/account/views.py
+++ d4o_social/account/views.py
@@ -74,3 +74,9 @@ def edit(request):
 def user_list(request):
     users = User.objects.filter(is_active=True)
     return render(request, 'account/user/list.html', {'section': 'people', 'users': users})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
