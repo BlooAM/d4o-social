f53ebee0a52bc7e5fd07b31cdf727d56f21605c7
BlooAM <adam3200@vp.pl>
2024-12-21 10:38:21 +0100

    cache incremets of total views

--- d4o_social/images/views.py
+++ d4o_social/images/views.py
@@ -4,10 +4,15 @@ from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
+import redis
 
 from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
+
+
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 @login_required
@@ -29,7 +34,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'selection': 'images', 'image': image})
+    total_views = r.incr(f'image:{image.id}.views')
+    return render(
+        request,
+        'images/image/detail.html',
+        {'selection': 'images', 'image': image, 'total_views': total_views},
+    )
 
 
 @login_required
